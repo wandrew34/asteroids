6ed8aa25f87fb713ea03a3c4011485c47865ad6f
wandrew34 <andrews.wesley@gmail.com>
2025-03-30 21:06:32 -0500

    2nd commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 
+from player import *
 from constants import *
 
 def main():
@@ -12,6 +13,9 @@ def main():
 
     pygame.init()
 
+    fpsclock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         pygame.Surface.fill(screen, (0,0,0))
@@ -19,6 +23,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        x = SCREEN_WIDTH / 2
+        y = SCREEN_HEIGHT /2
+
+        player = Player(x,y)
+        player.draw(screen)
+
+        dt = fpsclock.tick(60) / 1000
 
 
 if __name__ == "__main__":
